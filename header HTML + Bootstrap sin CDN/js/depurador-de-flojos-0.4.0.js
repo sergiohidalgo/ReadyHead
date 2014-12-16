73219f4cfacd88ebb13a1c9053154b0b9b79b6d8
Sergio Hidalgo <sergiohidalgo.94@gmail.com>
2014-12-16 17:48:03 -0300

    Compatibilidades IE8
    Ahora bootstrap es compatible con ie8, para esto e tenido que dejar de
    lado los CDN, pero bueno... es lo que hay y no es tan malo.. igual se
    sigue usando archivos min

--- header HTML + Bootstrap sin CDN/js/depurador-de-flojos-0.4.0.js
+++ header HTML + Bootstrap sin CDN/js/depurador-de-flojos-0.4.0.js
@@ -1,4 +1,4 @@
-function SEObyjQuery(){
+function depurandoTuFlojera(){
     //Aplicamos los title a las A
   	$('a').each(function(){
         var atributoTextdeA = $(this).attr('title');
@@ -22,8 +22,35 @@ function SEObyjQuery(){
                 $(this).attr('alt', nombreDelAlt);
             }
     });
+    
+    //Target si es sitio externo o termina en .pdf
+    /* Si el enlace termina en .pdf o comienza con http:// o https:// se aplica el target _blanck */
+    
+    $('a').each(function(){
+
+        var atributoTargetDeA = $(this).attr('target');
+        var URLdelHREF = $(this).attr('href');
+
+        URLdelHREF = URLdelHREF.split('/');
+        
+        if (atributoTargetDeA == undefined || atributoTargetDeA == ''){
+            
+            if(URLdelHREF[0] == 'http:' || URLdelHREF[0] == 'https:'){
+                $(this).attr('target','_blank');
+            }
+        }
+        
+        var extencionURL = URLdelHREF.pop();
+        extencionURL = extencionURL.split('.');
+        extencionURL = extencionURL.pop();
+
+        if(extencionURL == 'pdf'){
+            $(this).attr('target','_blank');
+        }
+        
+    });
 }
 
 jQuery(document).ready(function() {
-    SEObyjQuery();
+    depurandoTuFlojera();
 });
